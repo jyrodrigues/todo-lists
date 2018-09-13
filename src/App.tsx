698b7f47f8aba0af8871f5ba8ae35764ae371b8e
José Rodrigues <jyrodrigues@gmail.com>
2018-09-13 16:30:25 -0300

    Test emotion and add react-emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { css } from 'emotion'
 import * as React from 'react';
 import './App.css';
-
 import logo from './logo.svg';
+
+const myclass = css`
+  color: hotpink;
+  font-size: 1.5em;
+`
 
 class App extends React.Component {
   public render() {
@@ -9,7 +14,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className={myclass}>Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
