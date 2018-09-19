a25459ea03e82a819db4f3fa8ecd43b790e7e140
José Rodrigues <jyrodrigues@gmail.com>
2018-09-19 19:18:24 -0300

    Making tasks disappear with enums

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,7 +16,15 @@ type TaskListProps = {
 type TaskListState = {
     tasks: TaskData[],
     nextAvailableKey: number,
+    customTaskTitle: string,
+    selectedTasksToShow: TaskSelection,
 };
+
+enum TaskSelection {
+    All = "ALL",
+    Done = "DONE",
+    Todo = "TODO",
+}
 
 
 
@@ -32,6 +40,12 @@ const mystyle = css`
     overflow: scroll;
 `;
 
+const customTaskWrapperStyle = css`
+    margin: 20px;
+    padding: 20px;
+    border: solid 1px rgba(0,0,0,0.1);
+    border-radius: 4px;
+`;
 
 
 /* COMPONENT */
@@ -43,10 +57,17 @@ class TaskList extends React.Component<TaskListProps, TaskListState> {
         this.state = {
             tasks: this.props.tasks || [],
             nextAvailableKey: this.props.tasks.length,
+            customTaskTitle: '',
+            selectedTasksToShow: TaskSelection.All,
         }
 
         binder(this);
     }
+
+
+
+    /* SUBCOMPONENTS CPI Callbacks */
+
 
     public taskCallbacks(taskKey : number) : TaskCallbacks {
         return {
@@ -72,31 +93,87 @@ class TaskList extends React.Component<TaskListProps, TaskListState> {
         };
     }
 
+
+
+    /* COMPONENT LOGIC */
+
+
+    public handleNewTaskChange(e : React.FormEvent<HTMLInputElement>) : void {
+        this.setState({ customTaskTitle: e.currentTarget.value });
+    }
+
     public addRandomTask() : void {
+        this.addTask("Random task title");
+    }
+
+    public addNewTask(_: React.MouseEvent<HTMLButtonElement>) : void {
+        if (this.state.customTaskTitle.length === 0) { return; }
+        
+        this.addTask(this.state.customTaskTitle);
+    }
+
+    public addTask(title: string) : void {
         const newTask = {
-            title: "Random data",
+            title,
             done: false,
             key: this.state.nextAvailableKey,
         };
+        const tasks = [newTask].concat(this.state.tasks.slice());
 
-        const tasks = this.state.tasks.slice();
-        tasks.push(newTask);
         this.setState({
             tasks,
             nextAvailableKey: this.state.nextAvailableKey + 1,
         });
+
     }
+
+    public toggleTasksSelectionTo (showSelection : TaskSelection) : React.MouseEventHandler<HTMLButtonElement> {
+        return (e : React.MouseEvent<HTMLButtonElement>) => {
+            this.setState({ selectedTasksToShow: showSelection })
+        }
+    }
+
+    public prepareTasks () : JSX.Element[] {
+        return this.state.tasks.filter(taskData => {
+            switch (this.state.selectedTasksToShow) {
+                case TaskSelection.Done:
+                    return taskData.done;
+                
+                case TaskSelection.Todo:
+                    return !taskData.done;
+
+                case TaskSelection.All:
+                    return true;
+            }
+        })
+        .map((task : TaskData) => {
+            const taskProps : TaskProps = {...task, ...this.taskCallbacks(task.key)};
+            return <Task {...taskProps} />;
+        });
+    }
+
+
+
+    /* RENDER */
+
 
     public render() {
         return (
             <div>
+                <div className={customTaskWrapperStyle}>
+                    <input type="text" onChange={this.handleNewTaskChange}/>
+                    <button className={css`margin: 10px`} onClick={this.addNewTask}>Add</button>
+                </div>
                 <div className={mystyle}>
-                    {this.state.tasks.map((task : TaskData) => {
-                        const taskProps : TaskProps = {...task, ...this.taskCallbacks(task.key)};
-                        return <Task {...taskProps} />;
-                    })}
+                    {this.prepareTasks()}
                 </div>
                 <button onClick={this.addRandomTask}>Add Random Task! :)</button>
+                <div>
+                    {Object.keys(TaskSelection).map( selection => {
+                        return <button key={selection} onClick={this.toggleTasksSelectionTo(TaskSelection[selection])}>{selection}</button>;
+                    })}
+                    <div>Showing {this.state.selectedTasksToShow} tasks.</div>
+                </div>
             </div>
         )
     }
